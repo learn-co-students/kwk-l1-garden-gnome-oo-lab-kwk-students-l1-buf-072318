c9d543ddc4dfda78ae0a9cc6dc57b2495c123ee6
brysona10 <addisonbryson10@gmail.com>
2018-07-30 14:02:33 +0000

    Done.

--- garden_gnome.rb
+++ garden_gnome.rb
@@ -1,1 +1,15 @@
 # Code your instances here
+class GardenGnome
+  attr_accessor :name, :age, :gluten_allergy, :personality, :hat_color
+  def initialize(hat_color = "red")
+    @personality = "evil"
+    @hat_color = :hat_color
+  end
+end 
+
+
+
+
+
+
+  
